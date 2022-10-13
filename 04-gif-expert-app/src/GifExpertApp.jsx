54569d35b0d7122d9f4595cc49e6e4f1725e5d9d
Federico Mazzei <federicomazzei@gmail.com>
2022-10-13 16:41:55 -0300

    empiezo el proyecto gif-expert-app

---
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,14 @@
+//rafc Para crear un funcional component
+
+export const GifExpertApp = () => {
+  return (
+    <>
+    {/* titulo */}
+      <h1>GifExpertApp</h1>
+    {/* Input */}
+
+    {/* Listado de Gif */}
+      {/* Gif Item */}
+    </>
+  )
+}
