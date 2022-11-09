b41a85fc3f67199dd218ae96eff8733b58e3af26
Federico Mazzei <federicomazzei@gmail.com>
2022-11-09 15:50:50 -0300

    108 Hacer un mock completo de un Custom Hook

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,30 +1,25 @@
-import { useEffect, useState } from 'react';
-import { useFetchGifs } from '../hooks/useFetchGifs';
-import { GifItem } from './GifItem';
+import { useEffect, useState } from "react";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GifItem } from "./GifItem";
+import PropTypes from "prop-types";
 // import { getGifs } from "./helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-
-  const { images, isLoading } = useFetchGifs( category );
+  const { images, isLoading } = useFetchGifs(category);
 
   return (
     <>
-      <h3>{ category }</h3>
-      {
-        isLoading && (<h2>Cargando...</h2>)
-      }
+      <h3>{category}</h3>
+      {isLoading && <h2>Cargando...</h2>}
       <div className="card-grid">
-        {
-          images.map( (image) => (
-            <GifItem 
-                key={ image.id } 
-                {...image}
-            />
-          ))
-        }
+        {images.map((image) => (
+          <GifItem key={image.id} {...image} />
+        ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
+};
---
+++ 04-gif-expert-app/tests/components/GifGrid.test.jsx
@@ -0,0 +1,46 @@
+import { render, screen } from "@testing-library/react";
+import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock("../../src/hooks/useFetchGifs");
+
+describe("Pruebas en <GifGrid />", () => {
+  const category = "One Punch";
+
+  test("Debe de mostrar el loding inicial ", () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
+
+    render(<GifGrid category={category} />);
+    //    screen.debug();
+
+    expect(screen.getByText("Cargando..."));
+    expect(screen.getByText(category));
+  });
+
+  test("Debe de mostrar ítems cuando se cargan las imágenes mediante el usefetchGifs", () => {
+    const gifs = [
+      {
+        id: "ABC",
+        title: "Saitama",
+        url: "http://localhost/saitama.jpg",
+      },
+      {
+        id: "123",
+        title: "Goku",
+        url: "http://localhost/goku.jpg",
+      },
+    ];
+
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false,
+    });
+    render(<GifGrid category={category} />);
+    expect(screen.getAllByRole("img").length).toBe(2);
+
+    //screen.debug();
+  });
+});
