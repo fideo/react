fe94ad0d910357ed657f91be9c0d2e8283b1868c
Federico Mazzei <federicomazzei@gmail.com>
2022-12-08 11:42:27 -0300

    05-hook-app

---
+++ 05-hook-app/src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,11 @@
+export const CounterWithCustomHook = () => {
+  return (
+    <>
+      <h1>Counter With Hook</h1>
+      <hr />
+      <button className="btn btn-primary">+1</button>
+      <button className="btn btn-secondary">Reset</button>
+      <button className="btn btn-primary">-1</button>
+    </>
+  );
+};
