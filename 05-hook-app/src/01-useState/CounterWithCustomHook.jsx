fa4c83ffabbd6a413f302cac5aae63117cfa0cd8
Federico Mazzei <federicomazzei@gmail.com>
2022-12-21 09:04:25 -0300

    Agrego otras cositas

--- 05-hook-app/src/01-useState/CounterWithCustomHook.jsx
+++ 05-hook-app/src/01-useState/CounterWithCustomHook.jsx
@@ -1,11 +1,21 @@
+import { useCounter } from "../hooks/useCounter";
+
 export const CounterWithCustomHook = () => {
+  const { counter, increment, decrement, reset } = useCounter();
+
   return (
     <>
-      <h1>Counter With Hook</h1>
+      <h1>Counter With Hook: {counter}</h1>
       <hr />
-      <button className="btn btn-primary">+1</button>
-      <button className="btn btn-secondary">Reset</button>
-      <button className="btn btn-primary">-1</button>
+      <button className="btn btn-primary" onClick={increment}>
+        +1
+      </button>
+      <button className="btn btn-secondary" onClick={reset}>
+        Reset
+      </button>
+      <button className="btn btn-primary" onClick={decrement}>
+        -1
+      </button>
     </>
   );
 };
