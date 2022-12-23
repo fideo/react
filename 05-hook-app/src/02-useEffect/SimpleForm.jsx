48b2c6ec461675ea68a4260fedc5db3a71d7cab7
Federico Mazzei <federicomazzei@gmail.com>
2022-12-23 15:53:39 -0300

    avanzo con el simple form

--- 05-hook-app/src/02-useEffect/SimpleForm.jsx
+++ 05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -1,1 +1,32 @@
+import { useState } from "react"
+
+export const SimpleForm = () => {
+
+  const [formState, setformState] = useState({
+    username: 'strider',
+    email: 'federicomazzei@gmail.com'
+  })
+
+  return (
+    <>
+      <h1>SimpleForm</h1>
+      <hr />
+
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Username"
+        name="username"
+      />
+
+      <input
+        type="email"
+        className="form-control mt-2"
+        placeholder="federicomazzei@gmail.com"
+        name="email"
+      />
+      
+    </>
+  )
+}
 
